c7fe78c0e2864fa4d82ffeac1f1b1ba629e3367c
Sumanth Chinthagunta <xmlking@gmail.com>
2020-02-05 14:23:36 -0800

    chore(deps): updated nrwl/nx to 9.0.0-beta.4

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@yeti/gen';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
+  getData(): { message: string } {
     return { message: 'Welcome to api!' };
   }
 }
